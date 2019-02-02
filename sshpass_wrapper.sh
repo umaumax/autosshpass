87dfbe18b145b05796aff83659c020e7b85c7d6c
umaumax <umauma7083@gmail.com>
2019-02-03 01:07:39 +0900

    Refactor

--- sshpass_wrapper.sh
+++ sshpass_wrapper.sh
@@ -11,16 +11,29 @@
 
 function main() {
 	# NOTE: this filepath
-	local now_ssh_cmd=$0
-	local next_ssh_cmd=$(which -a $(basename $now_ssh_cmd) | tr ':' '\n' | grep -F $(dirname $now_ssh_cmd) -A2 | sed -n 2,2p)
-	[[ -z $next_ssh_cmd ]] && echo 1>&2 "Not found original command!" && return 1
+	local now_cmd_path=$0
+	local now_cmd=$(basename $now_cmd_path)
+	local next_cmd_path=$(which -a $now_cmd | tr ':' '\n' | grep -F $(dirname $now_cmd_path) -A2 | sed -n 2,2p)
+	if [[ -z $next_cmd_path ]]; then
+		if ! type >/dev/null 2>&1 $now_cmd; then
+			echo 1>&2 "Not found original '$now_cmd' command!"
+			return 1
+		fi
+		local next_cmd_path=$now_cmd
+	fi
 
-	local login_password=$($next_ssh_cmd -G "$@" | grep -A1 '^sendenv SSHPASS$' | sed -n 2,2p | cut -d' ' -f2-)
+	# NOTE: e.g. scp or rsync ?
+	local ssh_cmd='ssh'
+	if [[ $now_cmd == "ssh" ]]; then
+		local ssh_cmd="$next_cmd_path"
+	fi
+
+	local login_password=$($ssh_cmd -G "$@" 2>/dev/null | grep -A1 '^sendenv SSHPASS$' | sed -n 2,2p | cut -d' ' -f2-)
 	[[ -n $login_password ]] && export SSHPASS=$login_password
 	if type >/dev/null 2>&1 sshpass && [[ -n $SSHPASS ]]; then
-		sshpass -e "$next_ssh_cmd" "$@"
+		sshpass -e "$next_cmd_path" "$@"
 	else
-		"$next_ssh_cmd" "$@"
+		"$next_cmd_path" "$@"
 	fi
 }
 main "$@"
